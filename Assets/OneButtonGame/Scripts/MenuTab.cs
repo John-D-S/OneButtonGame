f6e4ae5ce410afe269761e8a69becce1ec103728
John-D-S <68535837+John-D-S@users.noreply.github.com>
2021-04-29 13:01:54 +1000

    started menuTab scripts

--- Assets/OneButtonGame/Scripts/MenuTab.cs
+++ Assets/OneButtonGame/Scripts/MenuTab.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.UI
 
 public class MenuTab : MonoBehaviour
 {
+    public int depthInMenuHeighrachy;
+
+    private HorizontalLayoutGroup TabLayout;
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/OneButtonGame/Scripts/MenuTabManager.cs
+++ Assets/OneButtonGame/Scripts/MenuTabManager.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class MenuTabManager : MonoBehaviour
 {
+    private List<MenuTab> menuTabs = new List<MenuTab>();
+    private MenuTab activeTab;
+
+    private int highestNumberInHeighrachy = 0;
+
     // Start is called before the first frame update
     void Start()
     {
+        //adding all the menu tab components in the children of this to the menuTabs list;
+        foreach (MenuTab menuTab in transform.GetComponentsInChildren<MenuTab>())
+            menuTabs.Add(menuTab);
+
+        foreach (MenuTab menuTab in menuTabs)
+        {
+            if menuTab.
+        }
+
         
+    }
+
+    private void IndentTab()
+    {
+        for
     }
 
     // Update is called once per frame
