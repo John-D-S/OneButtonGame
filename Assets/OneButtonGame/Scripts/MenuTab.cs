cf0b3e7fcce536ee23e6d0bef8382763ce51ce91
John <68535837+John-D-S@users.noreply.github.com>
2021-04-29 22:51:39 +1000

    Tab indentation and functionality added and partially implemented

--- Assets/OneButtonGame/Scripts/MenuTab.cs
+++ Assets/OneButtonGame/Scripts/MenuTab.cs
@@ -2,23 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
-using UnityEngine.UI
+using UnityEngine.UI;
 
 public class MenuTab : MonoBehaviour
 {
     public int depthInMenuHeighrachy;
+    [System.NonSerialized] public List<MenuTab> childTabs = new List<MenuTab>();
 
     private HorizontalLayoutGroup TabLayout;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        TabLayout = gameObject.GetComponent<HorizontalLayoutGroup>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ApplyIndent(int unitsPerIndent)
     {
-        
+        TabLayout.padding.left = depthInMenuHeighrachy * unitsPerIndent;
+    }
+
+    public void Collapse()
+    {
+        if (childTabs.Count > 0)
+            foreach (MenuTab tab in childTabs)
+                tab.Deactivate();
+    }
+
+    public void Expand()
+    {
+        if (childTabs.Count > 0)
+            foreach (MenuTab tab in childTabs)
+                tab.gameObject.SetActive(true);
+    }
+
+    public void Deactivate()
+    {
+        Collapse();
+        gameObject.SetActive(false);
+    }
+
+    public void Activate()
+    {
+        gameObject.SetActive(true);
+        Expand();
     }
 }
--- Assets/OneButtonGame/Scripts/MenuTabManager.cs
+++ Assets/OneButtonGame/Scripts/MenuTabManager.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class MenuTabManager : MonoBehaviour
 {
     private List<MenuTab> menuTabs = new List<MenuTab>();
-    private MenuTab activeTab;
+    private int activeTab = 0;
 
-    private int highestNumberInHeighrachy = 0;
+    [SerializeField] private int indentAmount = 100;
 
     // Start is called before the first frame update
     void Start()
@@ -16,17 +16,37 @@ public class MenuTabManager : MonoBehaviour
         foreach (MenuTab menuTab in transform.GetComponentsInChildren<MenuTab>())
             menuTabs.Add(menuTab);
 
+
+        //the foreach loop will iterate over each menuTab in the list and will assign it to the int in the childAssignmentDict correlating to its depthInMenuHeighrachy
+        //it will then add that menuTab into the childTabs list of the menuTab from the childAssignmentDict who's key is 1 less than that of the current menuTab.
+        Dictionary<int, MenuTab> childAssignmentDict = new Dictionary<int, MenuTab>();
+        MenuTab LastTab = null;
         foreach (MenuTab menuTab in menuTabs)
         {
-            if menuTab.
+            int menuTabDepth = menuTab.depthInMenuHeighrachy;
+            
+            if (LastTab)
+                if (menuTabDepth > LastTab.depthInMenuHeighrachy + 1)
+                    menuTab.depthInMenuHeighrachy = LastTab.depthInMenuHeighrachy + 1;
+
+            childAssignmentDict[menuTabDepth] = menuTab;
+
+            if (menuTabDepth > 0 && childAssignmentDict.ContainsKey(menuTabDepth + 1))
+                childAssignmentDict[menuTabDepth - 1].childTabs.Add(menuTab);
+            
+            LastTab = menuTab;
         }
 
-        
-    }
+        //apply indents to all the tabs
+        foreach (MenuTab tab in menuTabs)
+        {
+            tab.ApplyIndent(indentAmount);
+        }
 
-    private void IndentTab()
-    {
-        for
+        //collapse all the highest level menu tabs to start out
+        //foreach (MenuTab tab in menuTabs)
+        //    if (tab.depthInMenuHeighrachy = 0 &&)
+                
     }
 
     // Update is called once per frame
